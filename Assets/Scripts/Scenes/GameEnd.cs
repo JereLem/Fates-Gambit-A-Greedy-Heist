d36893538ed787a1fdfdda1e9c0cde69f2e223b2
JereLem <56977360+JereLem@users.noreply.github.com>
2024-03-01 22:47:55 +0200

    Jail, Win screens work now with animations & Other tweaks

--- Assets/Scripts/Scenes/GameEnd.cs
+++ Assets/Scripts/Scenes/GameEnd.cs
@@ -5,36 +5,73 @@ using TMPro;
 
 public class GameEnd : MonoBehaviour
 {
+    // Panels
+    public GameObject winPanel;
+    public GameObject losePanel;
+
+    private GameObject activePanel;
+
+    [SerializeField] public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Get UI elements and assign values
-        Transform endScreen = GameObject.Find("EndScreen").transform;
-        TMP_Text pedestriansPickpocketed = endScreen.Find("pedestriansPickpocketed").GetComponent<TMP_Text>();
-        pedestriansPickpocketed.text = GameStats.pedestriansPickpocketed.ToString();
-
-        TMP_Text pickpocketedValue = endScreen.Find("pickpocketedValue").GetComponent<TMP_Text>();
-        pickpocketedValue.text = GameStats.pickpocketedValue.ToString();
-
-        TMP_Text gameDuration = endScreen.Find("GameDuration").GetComponent<TMP_Text>();
-        gameDuration.text = GameStats.GameDuration.ToString();
-
-        TMP_Text gameOverReason = endScreen.Find("GameOverReason").GetComponent<TMP_Text>();
+        // Get the Parent gameobject
+        // Determine the active panel based on the GameOverReason
+        activePanel = null;
+        string gameOverText = "";
+        
+        // Set delay
+        float delay = 1.0f;
 
         switch (GameStats.GameOverReason)
         {
             case GameOverReason.PlayerCaught:
-                gameOverReason.text = "You got caught!";
+                animator.SetBool("lost", true);
+                activePanel = losePanel;
+                gameOverText = "You got caught!";
                 break;
             case GameOverReason.RanOutOfTime:
-                gameOverReason.text = "You ran out of time!";
+                animator.SetBool("lost", true);
+                activePanel = losePanel;
+                gameOverText = "You ran out of time!";
                 break;
             case GameOverReason.WonTheGame:
-                gameOverReason.text = "Awesome! You won the game!";
+                animator.SetBool("win", true);
+                activePanel = winPanel;
+                gameOverText = "Awesome! You won the game!";
                 break;
             default:
                 break;
         }
+
+        if (activePanel != null)
+        {
+            // Activate the panel
+            float activationDelay = delay;
+            Invoke("ActivatePanel", activationDelay);
+
+            // Get UI elements and assign values within the active panel
+            TMP_Text pedestriansPickpocketed = activePanel.transform.Find("pedestriansPickpocketed").GetComponent<TMP_Text>();
+            pedestriansPickpocketed.text = GameStats.pedestriansPickpocketed.ToString();
+
+            TMP_Text pickpocketedValue = activePanel.transform.Find("pickpocketedValue").GetComponent<TMP_Text>();
+            pickpocketedValue.text = GameStats.pickpocketedValue.ToString();
+
+            TMP_Text gameDuration = activePanel.transform.Find("GameDuration").GetComponent<TMP_Text>();
+            gameDuration.text = GameStats.GameDuration.ToString();
+
+            TMP_Text gameOverReason = activePanel.transform.Find("GameOverReason").GetComponent<TMP_Text>();
+            gameOverReason.text = gameOverText;
+        }
     }
 
+    // Method to activate the panel
+    void ActivatePanel()
+    {
+        if (activePanel != null)
+        {
+            activePanel.SetActive(true);
+        }
+    }
 }
