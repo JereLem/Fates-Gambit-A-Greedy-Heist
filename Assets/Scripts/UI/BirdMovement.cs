7dce5909e0b337bda237a8d029a9bce6d0f7657e
Eva Zorman <katalenca@gmail.com>
2024-03-02 17:06:57 +0200

    hotfix bird movement

--- Assets/Scripts/UI/BirdMovement.cs
+++ Assets/Scripts/UI/BirdMovement.cs
@@ -11,22 +11,27 @@ public class BirdMovement : MonoBehaviour
     private float imageWidth;
     private Image birdImage;
 
+    private int f = 0;
+    private int birdFrameYChange = 60;
+
     private void Awake()
     {
         birdImage = GetComponent<Image>();
         imageWidth = birdImage.sprite.rect.width;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector2 newPos;
         float newY = transform.position.y + Random.Range(minRange, maxRange);
 
+        if (f % birdFrameYChange != 0)
+            newY = transform.position.y;
+        
         if (transform.position.x + imageWidth / 2 < 0)
         {
             if (transform.position.y > 0 || transform.position.y < 0)
-                            newY = Screen.height / 2 + Random.Range(-30, 30);
+                            newY = Screen.height / 2 + Random.Range(-100, 100);
                 
                         newPos = new Vector2(Screen.width + imageWidth, newY);
         }
@@ -34,5 +39,6 @@ public class BirdMovement : MonoBehaviour
             newPos = new Vector2(transform.position.x - birdSpeed / birdDistance, newY);
 
         transform.position = newPos;
+        f = (f + 1) % birdFrameYChange;
     }
 }
